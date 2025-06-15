0d3adfd4e5b89555b609815d03852949750519f7
MichaelRohweder <michael.rohweder@protonmail.com>
2025-06-15 15:28:39 -0400

    testing zeffy

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import DonateQR from '../assets/donationQR.png';
-import { useEffect } from 'react';
 
 export default function Donate() {
+    const [scriptLoaded, setScriptLoaded] = useState(false);
+
     useEffect(() => {
         const script = document.createElement('script');
-        script.src = "https://zeffy-scripts.s3.ca-central-1.amazonaws.com/embed-form-script.min.js";
+        script.src = 'https://zeffy-scripts.s3.ca-central-1.amazonaws.com/embed-form-script.min.js';
         script.async = true;
+        script.onload = () => {
+            setScriptLoaded(true);
+        };
         document.body.appendChild(script);
-
-        
+        return () => document.body.removeChild(script);
     }, []);
+
     return (
         <main>
             <div className="container">
@@ -18,24 +22,23 @@ export default function Donate() {
                     <div className="roboCards">
                         <div className="robocard">
                             <div className="robocard-header">Donations to RoboTZ</div>
-                            <span>Your support for our nonprofit robotics program is crucial to bringing STEM opportunities to the youth of our community.<br />
+                            <span>
+                                Your support for our nonprofit robotics program is crucial to bringing STEM opportunities to the youth of our community.<br />
                                 Many RoboTZ students have gone on to work/study and further their education in engineering, mechanical, and computer science fields.<br />
                                 Your contribution today makes a difference for the future of tomorrow.<br /><br />
                                 Thank you for your RoboTZ support!
                             </span><br /><br />
 
-                            <div
-                                className="zeffy-button"
-                                data-form-id="e74405bb-b48a-4b73-ae13-8996a89bb3a3"
-                                data-color="#1c75bc"
-                                data-shape="rect"
-                                data-size="large"
-                            ></div>
+                            {scriptLoaded && (
+                                <div
+                                    className="zeffy-button"
+                                    data-form-id="e74405bb-b48a-4b73-ae13-8996a89bb3a3"
+                                    data-color="#1c75bc"
+                                    data-shape="rect"
+                                    data-size="large"
+                                ></div>
+                            )}
 
-
-                            <button
-                                className="donateButton"
-                                zeffy-form-link="https://www.zeffy.com/embed/donation-form/e74405bb-b48a-4b73-ae13-8996a89bb3a3?modal=true">Donate Now</button>
                             <br /><br />
                             OR<br /><br />
                             <img src={DonateQR} alt="Donation QR Code" />
