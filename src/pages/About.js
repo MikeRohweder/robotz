acb45d8df26909f5fc1e2938ea12e972ebc5bedd
MikeRohweder <michael.rohweder@protonmail.com>
2026-07-14 11:07:36 -0400

    Update About.js
    test2

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,7 +10,7 @@ export default function About() {
                 <p>Students apply innovative STEM concepts to design, build, and code competitive robots.</p>
                 <p>The team of RoboTZ coaches/mentors guide students through concepts of construction, coding, and the engineering process.</p>
                 <p>Our program offers a valuable experience for our RoboTZ students (a.k.a the problem-solving heroes of tomorrow)</p>
-                <br /><br />TEST
+                <br /><br />TEST123
             </section>
         </main>
     );
